214a101de5b79b04ea9fa2988db37f40b6817e10
linnykerxd9 <linnykerxd9@gmail.com>
2022-01-20 15:55:38 -0300

    Commit initial

---
+++ DesafioFornecedores.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DesafioFornecedores.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ DesafioFornecedores.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DesafioFornecedores.Infra
+{
+    public class Class1
+    {
+    }
+}
